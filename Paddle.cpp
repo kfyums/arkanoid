82dc2e40be83f775e8ce636f86fb3b4c1f42fece
kfyums <kfyums@gmail.com>
2026-06-13 23:46:51 +0300

    Update Paddle.cpp

--- Paddle.cpp
+++ Paddle.cpp
@@ -2,17 +2,19 @@
 
 Paddle::Paddle()
 {
-    shape.setSize(sf::Vector2f(120, 20));
+    shape.setSize(sf::Vector2f(120.f, 20.f));
     shape.setFillColor(sf::Color::Blue);
-    shape.setPosition(340, 550);
+    shape.setPosition(340.f, 550.f);
 }
 
 void Paddle::moveLeft()
 {
-    shape.move(-0.5f, 0);
+    if (shape.getPosition().x > 0)
+        shape.move(-0.5f, 0);
 }
 
 void Paddle::moveRight()
 {
-    shape.move(0.5f, 0);
+    if (shape.getPosition().x + shape.getSize().x < 800.f)
+        shape.move(0.5f, 0);
 }
